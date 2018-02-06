f50efa49b4f105ebc86de2f56785b282f36ca041
molo1134 <molo1134@gmx.com>
2018-02-06 11:10:07 -0500

    add support for alaska, group C and district 10

--- getlastsequenial.sh
+++ getlastsequenial.sh
@@ -5,6 +5,22 @@ if [ ! "$DISTRICT" ]; then
 	DISTRICT='1'
 fi
 
+if [ "$DISTRICT" -eq "10" ]; then
+	DISTRICT='0'
+fi
+
+A="A[^HL-Z]${DISTRICT}[A-Z]{2}\\>"
+B="K[^HLP]${DISTRICT}[A-Z]{2}\\>"
+D="K[^HLP]${DISTRICT}[A-Z]{3}\\>"
+
+# Alaska
+if [ "$DISTRICT" -eq "11" ]; then
+	A="[AKNW]L[0-9][A-Z]\\>"
+	B="AL[0-9][A-Z]{2}\\>"
+	C="[KNW]L[0-9][A-Z]{2}\\>"
+	D="[KNW]L[0-9][A-Z]{3}\\>"
+fi
+
 
 DATAFILE="$HOME/tmp/amat_hs.dat.gz"
 
@@ -12,20 +28,32 @@ LANG=C
 export LANG
 
 echo -n "D: "
-gzip -dc $DATAFILE | \
-	egrep "K[^HLP]${DISTRICT}[A-Z]{3}\\>.*SYSGRT" | \
-	tail -1 | \
+gzip -dc $DATAFILE					| \
+	egrep "${D}.*SYSGRT"				| \
+	sort -n -t "|" -k 2				| \
+	tail -1						| \
 	awk -F '|' '{print $4}'
 
+if [ "$C" ]; then
+	echo -n "C: "
+	gzip -dc $DATAFILE					| \
+		egrep "${C}.*SYSGRT"				| \
+		sort -n -t "|" -k 2				| \
+		tail -1						| \
+		awk -F '|' '{print $4}'
+fi
+
 echo -n "B: "
-gzip -dc $DATAFILE | \
-	egrep "K[^HLP]${DISTRICT}[A-Z]{2}\\>.*SYSGRT" | \
-	tail -1 | \
+gzip -dc $DATAFILE					| \
+	egrep "${B}.*SYSGRT"				| \
+	sort -n -t "|" -k 2				| \
+	tail -1						| \
 	awk -F '|' '{print $4}'
 
 echo -n "A: "
-gzip -dc $DATAFILE | \
-	egrep "A[^HL-Z]${DISTRICT}[A-Z]{2}\\>.*SYSGRT" | \
-	tail -1 | \
+gzip -dc $DATAFILE					| \
+	egrep "${A}.*SYSGRT"				| \
+	sort -n -t "|" -k 2				| \
+	tail -1						| \
 	awk -F '|' '{print $4}'
 
